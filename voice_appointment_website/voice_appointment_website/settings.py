bfe898c7073c430235f4f7dab4b4d65d075f178f
Hrishikesh Acharyya <acharyya.hrishi@gmail.com>
2025-06-20 18:57:45 +0530

    added allowed hosts directly in settings

--- voice_appointment_website/voice_appointment_website/settings.py
+++ voice_appointment_website/voice_appointment_website/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'team-excelsior-1.onrender.com']
 HOST_NAME = os.getenv('HOST_NAME')
 
 
